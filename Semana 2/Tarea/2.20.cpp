8fa2d58469647f4a486c0f81957718214c27c12c
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-24 17:32:58 -0500

    Ejercicio 2.20

--- Semana 2/Tarea/2.20.cpp
+++ Semana 2/Tarea/2.20.cpp
@@ -10,5 +10,6 @@ int main()
   std::cout << "El diametro del círculo es: " << radio*2 << std::endl;
   std::cout << "La circunferencia del círculo es: " << radio*3.14159*2 << std::endl;
   std::cout << "El área del círculo es: " << 3.14159*radio*radio << std::endl;
+  
   return 0;
 }
