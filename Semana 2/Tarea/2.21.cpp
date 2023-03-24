475e6d21fa6b1c0f68d75a0d1c0fdd579917d5b6
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-24 16:58:06 -0500

    Ejercicio 2.21

---
+++ Semana 2/Tarea/2.21.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::cout << "  CCC   +      +"   << std::endl;
+  std::cout << " C      +      +"   << std::endl;
+  std::cout << "C     +++++  +++++" << std::endl;
+  std::cout << " C      +      +"   << std::endl;
+  std::cout << "  CCC   +      +"   << std::endl;
+  return 0;
+}
