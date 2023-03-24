c0744175f51cfcdea67291751e18d555f4e3e00f
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-24 16:47:37 -0500

    Crar carpeta Tareas

---
+++ Semana 2/Tarea/2.20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  int radio{0};
+ 
+  std::cout << "Ingrese el radio del círculo: ";
+  std::cin >> radio;
+
+  std::cout << "El diametro del círculo es: " << radio*2 << std::endl;
+  std::cout << "La circunferencia del círculo es: " << radio*3.14159*2 << std::endl;
+  std::cout << "El área del círculo es: " << 3.14159*radio*radio << std::endl;
+}
