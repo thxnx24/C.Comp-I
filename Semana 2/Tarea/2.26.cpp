09f49dd609b3a8eb0b713ec5946eb33c6bb92ece
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-24 19:17:05 -0500

    Ejercicio 2.26

---
+++ Semana 2/Tarea/2.26.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n" << std::endl;
+  return 0;
+}
