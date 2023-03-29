6ac4a9ff57867f40091050a25e4151a951b8292e
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-29 14:37:25 -0500

    Renombre de carpetas

--- Semana 2/Tarea/2.26.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n" << std::endl;
-  return 0;
-}
