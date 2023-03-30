f7f886a741351d966860db5a9526a51406193f37
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-29 21:31:45 -0500

    Ejercicio 3.9

--- Semana 2/Tarea_2/3.9.cpp
+++ Semana 2/Tarea_2/3.9.cpp
@@ -4,38 +4,45 @@
 
 int main()
 {
-    Account account1{"Jane Green", 50};
-    Account account2{"John Blue", -7};
+    Cuenta cuenta1{"Jane Green", 50};
+    Cuenta cuenta2{"John Blue", -7};
 
-    std::cout << "Cuenta 1: " << account1.obtener_nombre() << " el saldo es  $" << account1.obtener_saldo() << std::endl;
-    std::cout << "Cuenta 2: " << account2.obtener_nombre() << " el saldo es $" << account2.obtener_saldo();
+    std::cout << "Cuenta 1: " << cuenta1.obtener_nombre() << " el saldo es  $" << cuenta1.obtener_saldo() << std::endl;
+    std::cout << "Cuenta 2: " << cuenta2.obtener_nombre() << " el saldo es $" << cuenta2.obtener_saldo();
 
-    std::cout << "\n\nIntroducir el importe del depósito a la cuenta1: ";
     int depositAmount;
-    std::cin >> depositAmount;
-
-    std::cout << "añadiendo $" << depositAmount << " a la cuenta1 saldo $";
-    account1.deposito(depositAmount);
-
-
-    std::cout << "\n\nCuenta1: " << account1.obtener_nombre() << " el saldo es  $"<< account1.obtener_saldo();
-    std::cout << "\nCuenta2: " << account2.obtener_nombre() << " el saldo es  $" << account2.obtener_saldo();
-
-
     std::cout << "\n\nIntroducir el importe del depósito a la cuenta1: ";
+    std::cin >> depositAmount;
+    std::cout << "añadiendo $" << depositAmount << " a la cuenta1 saldo $";
+    cuenta1.deposito(depositAmount);
+
+    std::cout << "\n\nCuenta1: " << cuenta1.obtener_nombre() << " el saldo es  $"<< cuenta1.obtener_saldo();
+    std::cout << "\nCuenta2: " << cuenta2.obtener_nombre() << " el saldo es  $" << cuenta2.obtener_saldo();
+
+    std::cout << "\n\nIntroducir el importe del depósito a la cuenta2: ";
     std::cin >> depositAmount;
     std::cout << "añadiendo $" << depositAmount << " a la cuenta2 saldo $";
-    account2.deposito(depositAmount);
+    cuenta2.deposito(depositAmount);
 
+    std::cout << "\n\nCuenta1: " << cuenta1.obtener_nombre() << " el saldo es  $" << cuenta1.obtener_saldo(); 
+    std::cout << "\nCuenta2: " << cuenta2.obtener_nombre() << " el saldo es  $" << cuenta2.obtener_saldo() << std::endl; 
 
-    std::cout << "\n\nCuenta1: " << account1.obtener_nombre() << " el saldo es  $" << account1.obtener_saldo(); 
-    std::cout << "\nCuenta2: " << account2.obtener_nombre() << " el saldo es  $" << account2.obtener_saldo() << std::endl; 
-
-    
-    std::cout << "\nIntroduzca la cantidad a retirar de la cuenta1 por favor: ";
     int retiro_dinero;
+    std::cout << "\nIntroduzca la cantidad a retirar de la cuenta1 por favor: ";
     std::cin >> retiro_dinero;
     std::cout << "Retirando $" << retiro_dinero << " de la cuenta1: ";
-    account1.retirar(retiro_dinero);
+    cuenta1.retirar(retiro_dinero);
+
+    std::cout << "\n\nCuenta1: " << cuenta1.obtener_nombre() << " el saldo es  $"<< cuenta1.obtener_saldo();
+    std::cout << "\nCuenta2: " << cuenta2.obtener_nombre() << " el saldo es  $" << cuenta2.obtener_saldo();
+
+    std::cout << "\nIntroduzca la cantidad a retirar de la cuenta2 por favor: ";
+    std::cin >> retiro_dinero;
+    std::cout << "Retirando $" << retiro_dinero << " de la cuenta2: ";
+    cuenta2.retirar(retiro_dinero);
+
+    std::cout << "\n\nCuenta1: " << cuenta1.obtener_nombre() << " el saldo es  $"<< cuenta1.obtener_saldo();
+    std::cout << "\nCuenta2: " << cuenta2.obtener_nombre() << " el saldo es  $" << cuenta2.obtener_saldo();
+
 
 }
