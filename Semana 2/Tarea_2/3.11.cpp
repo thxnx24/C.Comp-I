ff1a9b81f13a5f939c69d8df1670c7a9a00532c7
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-03-30 01:47:59 -0500

    Ejercicio 2.11

---
+++ Semana 2/Tarea_2/3.11.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "3.11.h"
+
+int main()
+{ 
+    std::string marca;
+    std::string tipo_combustible;
+    int año_fabricacion;
+    std::string color;
+    int cilindrada;
+
+    std::cout << "Ingrese marca del vehículo: ";
+    std::cin >> marca;
+    std::cout << "Ingrese tipo de combustible del vehículo: ";
+    std::cin >> tipo_combustible;
+    std::cout << "Ingrese año de fabricación del vehículo: ";
+    std::cin >> año_fabricacion;
+    std::cout << "Ingrese color del vehículo: ";
+    std::cin >> color;
+    std::cout << "Ingrese la cilindrada del vehículo: ";
+    std::cin >> cilindrada;
+
+    Motor_vehiculo vehiculo(marca,tipo_combustible,año_fabricacion,color,cilindrada);
+    vehiculo.detalles_coche();
+    
+    return 0;
+}
---
+++ Semana 2/Tarea_2/3.11.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+class Motor_vehiculo {
+public:
+    Motor_vehiculo(std::string _marca, std::string _tipo_combustible, int _año_fabricacion, std::string _color, int _cilindrada) 
+        :marca(_marca), tipo_combustible(_tipo_combustible),año_fabricacion(_año_fabricacion),color(_color),cilindrada(_cilindrada) {}
+
+    void set_marca(std::string _marca)
+    {
+        marca = _marca;
+    }
+    std::string get_marca()
+    {
+        return marca;
+    }
+
+    void set_tipo_combustible(std::string _tipo_combustible)
+    {
+        tipo_combustible = _tipo_combustible;
+    }
+    std::string get_tipo_combustible() 
+    {
+        return tipo_combustible;
+    }
+
+    void set_año_fabricacion(int _año_fabricacion)
+    {
+        año_fabricacion = _año_fabricacion;
+    }
+    int get_año_fabricacion()
+    {
+        return año_fabricacion;
+    }
+
+    void set_color(std::string _color)
+    {
+        color = _color;
+    }
+    std::string get_color()
+    {
+        return color;
+    }
+
+    void set_cilindrada(int _cilindrada)
+    {
+        cilindrada = _cilindrada;
+    }
+    int get_cilindrada() 
+    {
+        return cilindrada;
+    }
+
+    void detalles_coche() 
+    {
+        std::cout << "\nDETALLES DEL COCHE:\n";
+        std::cout << "Marca: " << marca << std::endl;
+        std::cout << "Tipo de combustible: " << tipo_combustible << std::endl;
+        std::cout << "Año de fabricación: " << año_fabricacion << std::endl;
+        std::cout << "Color: " << color << std::endl;
+        std::cout << "Cilindrada: " << cilindrada << std::endl;
+    }
+
+private:
+    std::string marca;
+    std::string tipo_combustible;
+    int año_fabricacion;
+    std::string color;
+    int cilindrada;
+};
