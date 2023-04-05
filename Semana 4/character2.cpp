ffd899fa4872f0a51ec47b7ca70113809edc8df6
Alumno B303 <Alumno-B303@UCSP.AULAB>
2023-04-05 17:36:06 -0500

    character2.cpp

---
+++ Semana 4/character2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    char car1 , car2;
+    std::cout << "Ingrese el primer caracter: ";
+    std::cin >> car1;
+    std::cout << "Ingrese el segundo caracter: ";
+    std::cin >> car2;
+
+    int val1 = static_cast<int>(car1);
+    int val2 = static_cast<int>(car2);
+
+    if ( (val1 >= 48 && val1 <= 57) && (val2 >= 48 && val2 <= 57))
+    {
+
+    }
+    else
+    {
+        std::cout << "Uno o ambos caractereres no representan un número";
+    }
+
+
+}
