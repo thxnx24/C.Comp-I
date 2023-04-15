e28644e9da7686c84e5dedc236e09588ee821ad1
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-14 22:56:50 -0500

    problema1.cpp

--- Semana 5/Practica Laboratorio 3/problema1.cpp
+++ Semana 5/Practica Laboratorio 3/problema1.cpp
@@ -1,3 +1,8 @@
+/*Si enumeramos todos los números naturales debajo de 10 que son múltiplos de 3 o 5, obtenemos 3, 5, 6 y 9. 
+La suma de estos múltiplos es 23.
+Encuentra la suma de todos los múltiplos de 3 o 5 por debajo de 1000.
+*/
+
 #include <iostream>
 
 int main()
