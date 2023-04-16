95cf5eeebadd47b9ec6e7a60b6e6c37ea315afae
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-16 16:21:32 -0500

    problema10.cpp

---
+++ Semana 5/Practica Laboratorio 3/problema10.cpp
@@ -0,0 +1,11 @@
+/*La suma de los números primos por debajo de 10 es 2 + 3 + 5 + 7 = 17.
+
+Encuentra la suma de todos los números primos por debajo de dos millones.
+*/
+
+#include <iostream>
+
+int main()
+{
+    
+}
