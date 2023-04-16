6c350a8b23301c223efc21cff06c58d0c3fb92ac
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-16 12:18:05 -0500

    problema6.cpp

--- Semana 5/Practica Laboratorio 3/problema6.cpp
+++ Semana 5/Practica Laboratorio 3/problema6.cpp
@@ -1,1 +1,30 @@
+/*La suma de los cuadrados de los diez primeros números naturales es 385
+
+El cuadrado de la suma de los primeros diez números naturales es 3025
+
+Por tanto, la diferencia entre la suma de los cuadrados de los diez primeros números naturales 
+y el cuadrado de la suma es 2640
+
+Encuentra la diferencia entre la suma de los cuadrados de los primeros cien números naturales
+y el cuadrado de la suma.
+*/
+
 #include <iostream>
+
+int main()
+{
+    long long x{0};
+    long long y{0};
+    long long diferencia;
+    for (int i = 0; i <= 100; i++)
+    {
+        x += (i*i);
+        y += i; 
+    }
+    diferencia = (y*y) - x;
+    std::cout << "La diferencia entre la suma de cuadrados y el cuadrado de la suma es: " << diferencia;
+
+    return 0;
+
+}
+
