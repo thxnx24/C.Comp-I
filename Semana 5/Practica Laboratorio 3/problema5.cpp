1522019308bde535fa836e4b68cc0f3102fce232
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-16 12:09:31 -0500

    problema5.cpp

--- Semana 5/Practica Laboratorio 3/problema5.cpp
+++ Semana 5/Practica Laboratorio 3/problema5.cpp
@@ -1,3 +1,7 @@
+/*2520 es el número más pequeño que se puede dividir por cada uno de los números del 1 al 10 sin resto.
+¿Cuál es el número positivo más pequeño que es divisible por todos los números del 1 al 20?
+*/
+
 #include <iostream>
 
 long long maximmo_divisor( long long x,  long long y)
