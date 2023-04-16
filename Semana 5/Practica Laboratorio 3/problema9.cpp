d4ed6adc12d72330662ed8cacc76310c8bf3cf85
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-16 14:42:01 -0500

    problema9.cpp

--- Semana 5/Practica Laboratorio 3/problema9.cpp
+++ Semana 5/Practica Laboratorio 3/problema9.cpp
@@ -1,7 +1,7 @@
 /*Un triplete pitagórico es un conjunto de tres números naturales, a < b < c , para los cuales,
 un^2 + segundo^2 = do^2
 
-Por ejemplo, 3 2 + 4 2 = 9 + 16 = 25 = 5 2 .
+Por ejemplo, 3^2 + 4^2 = 9 + 16 = 25 = 5^2 
 
 Existe exactamente un triplete pitagórico para el cual a + b + c = 1000.
 Encuentra el producto abc .
