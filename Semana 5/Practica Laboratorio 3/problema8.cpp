adb859acfa7ee57088ce988250c466afc11e6d91
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-16 14:26:46 -0500

    problema8.cpp

--- Semana 5/Practica Laboratorio 3/problema8.cpp
+++ Semana 5/Practica Laboratorio 3/problema8.cpp
@@ -1,6 +1,11 @@
+/*Los cuatro dígitos adyacentes en el número de 1000 dígitos que tienen el mayor producto 
+son 9 × 9 × 8 × 9 = 5832.
+Encuentra los trece dígitos adyacentes en el número de 1000 dígitos que tienen el mayor producto. 
+¿Cuál es el valor de este producto?
+*/
+
 #include <iostream>
 #include <string>
-
 
 int main(){
     long long maximo{0};
