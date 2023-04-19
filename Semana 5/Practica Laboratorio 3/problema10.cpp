122a3bd3b11be3f11d05297397414280e089b2e5
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-18 22:09:38 -0500

    problema10.cpp

--- Semana 5/Practica Laboratorio 3/problema10.cpp
+++ Semana 5/Practica Laboratorio 3/problema10.cpp
@@ -5,7 +5,28 @@ Encuentra la suma de todos los números primos por debajo de dos millones.
 
 #include <iostream>
 
-int main()
+bool verificador (long long num) 
 {
-    
+    for (int i = 2; i * i <= num; i++) 
+    { 
+        if (num % i == 0) 
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() 
+{
+    long long suma{0};
+    for(int i = 2; i <= 2000000; i++) 
+    {
+        if(verificador(i))
+        {
+            suma = suma + i;
+        }
+    }
+    std::cout << "La suma de todos lo números primos hasta el 2000000 es: " << suma << std::endl;
+    return 0;
 }
