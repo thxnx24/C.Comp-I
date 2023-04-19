83779333dc06d0688fbcc1dc055d69671f7e9c2a
Alumno B303 <Alumno-B303@UCSP.AULAB>
2023-04-19 17:37:34 -0500

    main.cpp

--- Semana 6/main.cpp
+++ Semana 6/main.cpp
@@ -4,17 +4,17 @@
 
 int main()
 {
-    array<int, 5> n;
+    std::array <int,5> n;
 
     for (size_t i{0}; i < n.size(); ++i)
     {    
         n[i] = 0;
     }
-    std::cout << "Element" << setw(10) << "Value" << std::endl;
+    std::cout << "Element" << std::setw(10) << "Value" << std::endl;
 
     for (size_t j{0}; j < n.size(); ++j)
     {
-        std::cout << setw(7) << j << setw(10) <<n[j] << std::endl;
+        std::cout << std::setw(7) << j << std::setw(10) <<n[j] << std::endl;
     }
 
 
