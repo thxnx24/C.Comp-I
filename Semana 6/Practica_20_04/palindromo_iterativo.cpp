daccb821e9bcc0ea248e71ec720131d315f72d10
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-26 15:49:49 -0500

    Practica 20/04

--- Semana 6/Practica_20_04/palindromo_iterativo.cpp
+++ Semana 6/Practica_20_04/palindromo_iterativo.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 
-int palindromo(int arr[],  int tam);
-
-int main() 
-{
-    int arr[] = {1,2,3,2,2,3,4,1};
-    
-    palindromo(arr,8);
-    
-    return 0;
-}
-
-int palindromo(int arr[],  int tam)
+int palin(int array[],  int tam)
 {
     int t;
-    for(int i = 0; i < tam / 2; i++)
+    for(int i = 0; i < tam/2; i++)
     {
-        if(arr[i] == arr[tam-i-1])
+        if(array[i] == array[tam-i-1])
         {
             t = 1;
         }
-        else{
+        else
+        {
             t = 0;
         }    
     }
     if(t == 1)
     {
-        std::cout<<"El arreglo es palindromo";
+        std::cout <<"El arreglo es palindromo";
     }
     else
     {
-        std::cout<<"El arreglo no es palindromo";
+        std::cout <<"El arreglo no es palindromo";
     }
+    return 0;
+}
+
+int main()
+{
+    int array[] = {1,3,4,2,3,2,1};
+    
+    palin(array,7);
+    
     return 0;
 }
