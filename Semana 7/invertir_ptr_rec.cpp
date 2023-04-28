5d7b0fa48fd2c6600e31000a4afac2a2ea0915a2
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-04-27 19:29:20 -0500

    Semana 7

--- Semana 7/invertir_ptr_rec.cpp
+++ Semana 7/invertir_ptr_rec.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-
 void intercambio(int &a, int &b){
     int tmp = a;
     a = b;
     b = tmp;
 }
 
-void invertir_array(int* arr, int size) 
+void invertir_rec(int *ini, int *fin)
 {
-    int* ini = arr;
-    int* fin = arr + size - 1;
-    
-    while (ini < fin) 
+    if (ini >= fin)
     {
-        intercambio(*ini,*fin);
-        ini++;
-        fin--;
+        return;
     }
+    intercambio(*ini, *fin);
+    invertir_rec(++ini,--fin);
+
 }
+
 void imprimir(int arr[], int size)
 {
     for (int i{0}; i < size; i++)
@@ -25,16 +23,13 @@ void imprimir(int arr[], int size)
         std::cout << arr[i] << " ";
     }
 }
-
-int main() {
+int main() 
+{
     int arr[] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
-    
-    // Imprimir el arreglo original
     imprimir(arr,size);
     
-    // Invertir el arreglo
-    invertir_array(arr, size);
+    invertir_rec(arr, arr + size - 1);
     
     std::cout << std::endl;
     imprimir(arr,size);
