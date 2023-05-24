f659808484e876e533f0d16c69f92e54a8fabc39
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-05-24 18:01:21 -0500

    Semana 11

--- Semana 11/BasePlusCommissionEmployee2.cpp
+++ Semana 11/BasePlusCommissionEmployee2.cpp
@@ -45,26 +45,3 @@ string BasePlusCommissionEmployee::toString() const
         << "\nbase salary: " << baseSalary;
     return output.str();
 }
-
-int main() 
-{
-
-    BasePlusCommissionEmployee employee{"Bob", "Lewis", "333-33-3333",
-        5000, .04, 300};
-
-    cout << fixed << setprecision(2); // set floating-point formatting
-    cout << "Employee information obtained by get functions: \n"
-        << "\nFirst name is " << employee.getFirstName()
-        << "\nLast name is " << employee.getLastName()
-        << "\nSocial security number is " << employee.getSocialSecurityNumber()
-        << "\nGross sales is " << employee.getGrossSales()
-        << "\nCommission rate is " << employee.getCommissionRate()
-        <<  "\nBase salary is " << employee.getBaseSalary() << endl;
-
-    employee.setBaseSalary(1000);
-    
-    cout << "\nUpdated employee information from function toString: \n\n"
-        << employee.toString();
-
-    cout << "\n\nEmployee's earnings: $" << employee.earnings() << endl;
-}
