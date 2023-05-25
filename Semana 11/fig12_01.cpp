f4992bcede64dda7995bcc12028928f487004767
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-05-25 02:57:46 -0500

    Semana 11

--- Semana 11/fig12_01.cpp
+++ Semana 11/fig12_01.cpp
@@ -2,3 +2,42 @@
 
 #include <iostream>
 #include <iomanip>
+#include "CommissionEmployee.h"
+#include "BasePlusCommissionEmployee2.h"
+using namespace std;
+
+int main()
+{
+    CommissionEmployee commissionEmployee{
+        "Sue", "Jones", "222-22-2222", 1000, .06};
+
+    BasePlusCommissionEmployee basePlusCommissionEmployee{
+        "Bob", "Lewis", "333-33-3333", 5000, .04, 300};
+
+    cout << fixed << setprecision(2);
+
+    cout << "DISPLAY BASE-CLASS AND DERIVED CLASS OBJECTS:\n"
+        << commissionEmployee.toString()
+        << "\n\n"
+        << basePlusCommissionEmployee.toString();
+
+    CommissionEmployee* commissionEmployeePtr{&commissionEmployee};
+    cout << "\n\nCALLING TOSTRING WITH BASE-CLASS POINTER TO "
+        << "\nBASE-CLASS OBJECT INVOKES BASE-CLASS TOSTRING FUNCTION:\n"
+        << commissionEmployeePtr->toString();
+
+    BasePlusCommissionEmployee* basePlusCommissionEmployeePtr{
+        &basePlusCommissionEmployee};
+    cout << "\n\nCALLING TOSTRING WITH DERIVED-CLASS POINTER TO "
+        << "\nDERIVED-CLASS OBJECT INVOKES DERIVED-CLASS "
+        << "TOSTRING FUNCTION:\n"
+        << basePlusCommissionEmployeePtr->toString();
+    
+    commissionEmployeePtr = &basePlusCommissionEmployee;
+
+    cout << "\n\nCALLING TOSTRING WITH BASE-CLASS POINTER TO "
+        << "DERIVED-CLASS OBJECT\nINVOKES BASE-CLASS TOSTRING "
+        << "FUNCTION ON THAT DERIVED-CLASS OBJECT:\n"
+        << commissionEmployeePtr -> toString()
+        << endl;
+}
