f42c1d10b218a1570aaf2ef268ec64e2f2797026
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-05-25 19:31:17 -0500

    Semana 11

--- Semana 11/3/CommissionEmployee.cpp
+++ Semana 11/3/CommissionEmployee.cpp
@@ -77,24 +77,3 @@ string CommissionEmployee::toString() const
         << "\ncommission rate: " <<  commissionRate;
     return output.str();
 }
-
-int main()
-{
-    CommissionEmployee employee{"Sue","Jones","222-22-2222",10000, .06};
-
-    cout << fixed << setprecision(2);
-    cout << "Employee information obtained by get functions: \n"
-        << "\nFisrt name is " << employee.getFirstName()
-        << "\nLast name is " << employee.getLastName()
-        << "\nSocial security name is " 
-        << employee.getSocialSecurityNumber()
-        << "\nGross sales is " << employee.getGrossSales()
-        << "\nCommission rate is " <<employee.getCommissionRate() << endl;
-
-    employee.setGrossSales(8000);
-    employee.setCommissionRate(.1);
-    cout << "\nUpdated employee information from function toString: \n\n" 
-        << employee.toString();
-    
-    cout << "\n\nEmployee's earnings: $" << employee.earnings() << endl;
-}
